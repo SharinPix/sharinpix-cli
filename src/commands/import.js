c45341981afa1354de2d7794761586b8e03065f5
DJoyekurun <djoyekurun@outlook.com>
2018-09-28 16:25:16 +0400

    check csv syntax

--- src/commands/import.js
+++ src/commands/import.js
@@ -1,35 +1,52 @@
-const fs = require('fs')
-const { Command, flags } = require('@oclif/command')
-var _ = require('lodash')
-var csv = require('fast-csv')
+const { Command, flags } = require('@oclif/command'),
+      csv = require('fast-csv'),
+      fs = require('fs'),
+      _ = require('lodash')
 
 class ImportCommand extends Command {
   async run() {
-    const { flags } = this.parse(ImportCommand)
-    const file = flags.file
+    return new Promise((resolve,reject)=>{
+      const { flags } = this.parse(ImportCommand)
+      const file = flags.file
+
+      var readStream = fs.createReadStream(file),
+          album_id, url, tags, metadatas, json, errorFlag
+
+      csv
+      .fromStream(readStream, {delimiter : ';'})
+        .on('data', (data) => {
+
+          [album_id, url, tags, metadatas] = _.values(data)
+
+          if (album_id.length != 18){
+            console.error(new Error(`Album Id in ${file} should contain 18 characters!`))
+            //reject(Error(`Album Id in ${file} should contain 18 characters!`))
+          }
+
+          if (!metadatas || metadatas == ''){
+            metadatas ='{}'
+          }
+
+          try{
+            json = JSON.parse(metadatas)
+          }
+          catch(err){
+          }
+        })
+        .on('end', () => {
+          this.log(`Album Id: ${album_id}`)
+          this.log(`Album Id length: ${album_id.length}`)
+          this.log(`Url: ${url}`)
+          this.log(`Tags: ${tags}`)
+          this.log(`Metadatas: ${metadatas}`)
+          this.log(json)
+          resolve()
+        })
+        .on('error', (err) => {
+          console.log(err)
+        })
+    });
     
-    // csv
-    //   .fromPath(file, {headers: false})
-    //   .on('data', data => {
-    //     this.log(data)
-    //   })
-    //   .on('end', () => {
-        
-    //   })
-
-    var readStream = fs.createReadStream(file)
-
-    csv
-    .fromStream(readStream, {delimiter : ';'})
-      .on('data', (data) => {
-        var album_id, url, tags, metadatas
-        [album_id, url, tags, metadatas] = _.values(data)
-        
-        console.log(album_id.length)
-      })
-      .on('end', () => {
-        
-      })
   }
 }
 
--- test/commands/import.test.js
+++ test/commands/import.test.js
@@ -1,10 +1,22 @@
-const {expect, test} = require('@oclif/test')
+const {expect, test} = require('@oclif/test'),
+      fs = require('fs'),
+      csv = require('fast-csv'),
+      _ = require('lodash'),
+      assert = require("assert")
 
 describe('import', () => {
   test
   .stdout()
   .command(['import','--file','sample.csv'])
   .it('reads csv file', ctx => {
-    expect(ctx.stdout).to.contain('http')
+    assert.notEqual(-1, ctx.stdout.indexOf('Album Id length: 18'));
+    expect(ctx.stdout).to.contain('Metadatas: {}')
+  })
+
+  test
+  .stderr()
+  .command(['import','--file','sample_bad.csv'])
+  .it('AlbumId not 18 characters', ctx => {
+    expect(ctx.stderr).to.contain('Album Id in sample_bad.csv should contain 18 characters!')
   })
 })
